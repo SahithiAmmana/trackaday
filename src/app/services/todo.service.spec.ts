c39a7a3a05719b467ee1cd6ce8f339712adaedcc
Sahithi Ammana <49410550+mithila-reddy@users.noreply.github.com>
2022-12-05 02:51:45 -0500

    Update todo.service.spec.ts

--- src/app/services/todo.service.spec.ts
+++ src/app/services/todo.service.spec.ts
@@ -3,14 +3,23 @@ import { TestBed } from '@angular/core/testing';
 import { TodoService } from './todo.service';
 
 import { ToastrModule } from 'ngx-toastr';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../app.component';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 describe('TodoService', () => {
   let service: TodoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({imports: [
-      ToastrModule.forRoot() // added this works for me
-  ]});
+      ToastrModule.forRoot(), // added this works for me
+      AngularFireModule.initializeApp(environment.firebase),
+      RouterTestingModule
+  ],
+  providers: [AngularFireAuth, TodoService, AppComponent]
+  });
     service = TestBed.inject(TodoService);
   });
 
