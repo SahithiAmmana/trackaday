daa17ac9a3e7b3063bd143235f4d6ecf77bc30c3
Sahithi Ammana <49410550+mithila-reddy@users.noreply.github.com>
2022-12-05 03:24:17 -0500

    Update timer.service.spec.ts

--- src/app/services/timer.service.spec.ts
+++ src/app/services/timer.service.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+import { AppComponent } from '../app.component';
 import { TimerService } from './timer.service';
 
 describe('TimerService', () => {
   let service: TimerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+      ],
+      providers: [AngularFireAuth, AppComponent],
+    });
     service = TestBed.inject(TimerService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+
+    service.delay(10);
+    service.readDataStore()
+    service.save()
+    console.log(service.sessionList)
+
+    });
   });
-});
