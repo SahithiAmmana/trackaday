e23c510a46229a29338cead81e3eff156a2e272a
vdasi <vineethdasi22@gmail.com>
2022-12-05 02:22:05 -0500

    Update authentication.service.spec.ts

--- src/app/services/authentication.service.spec.ts
+++ src/app/services/authentication.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment';
+import { AuthenticationService } from "./authentication.service";
 
-import { AuthenticationService } from './authentication.service';
-
-describe('AuthenticationService', () => {
+describe("AuthenticationService", () => {
   let service: AuthenticationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+      AngularFireModule.initializeApp(environment.firebase),
+    ],
+    providers: [ 
+      AngularFireAuth,{ provide: ToastrService, useValue: ToastrService }
+    ]});
     service = TestBed.inject(AuthenticationService);
   });
 
-  it('should be created', () => {
+  describe('method1', () => {
+    it('should ...', () => {
     expect(service).toBeTruthy();
+
+    service.SignIn("vineethdasi1999@gmail.com","123456");
+    service.SignUp("vineeth","vineethtest@gmail.com","123456");
+    service.SignOut();
+
+
+    });
   });
 });
