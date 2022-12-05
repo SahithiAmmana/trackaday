cef592489ccdff5719ef0e7bb4a050e62824603e
Sahithi Ammana <112002925+SahithiAmmana@users.noreply.github.com>
2022-12-05 18:47:21 -0500

    Update todo test

--- src/app/components/todo/todo.component.spec.ts
+++ src/app/components/todo/todo.component.spec.ts
@@ -4,7 +4,6 @@ import { TodoComponent } from './todo.component';
 
 import { IndividualConfig, ToastrModule, ToastrService } from 'ngx-toastr';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TodoService } from 'src/app/services/todo.service';
 import { AppComponent } from 'src/app/app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
@@ -43,25 +42,4 @@ describe('TodoComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should delete when not complete', () => {
-    const timeStamp: TaskTimestamp = {
-      startTime: "12344",
-      endTime: "12345",
-      sessionId: "1"
-    }
-    const item: Todo = {
-      taskId: 1,
-      isCompleted: false,
-      isFavorite: false,
-      isPinned: false,
-      date: new Date().toISOString(),
-      title: "taskTitle",
-      isArchived: false,
-      timeStamps: [timeStamp]
-    }
-    component.todoService.todoList = [item];
-    component.isFavorite(item);
-    component.togglePin(item);
-    component.deleteTodo(item);
-  });
 });
