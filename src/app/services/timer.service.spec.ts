63186d932ab4ac89abc75b465705a8cd9177e4b0
Sai Pavan Yalla <112219214+SaiPavanYalla@users.noreply.github.com>
2022-12-05 07:13:08 -0500

    Updated tests

--- src/app/services/timer.service.spec.ts
+++ src/app/services/timer.service.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
 import { AppComponent } from '../app.component';
+import { Session } from '../models/session';
 import { TimerService } from './timer.service';
 
+describe('TimerService', () => {
 describe('TimerService', () => {
   let service: TimerService;
 
@@ -13,7 +16,7 @@ describe('TimerService', () => {
       imports: [
         AngularFireModule.initializeApp(environment.firebase),
       ],
-      providers: [AngularFireAuth, AppComponent],
+      providers: [AngularFireAuth, AppComponent, { provide: ToastrService, useValue: ToastrService }]
     });
     service = TestBed.inject(TimerService);
   });
@@ -21,10 +24,17 @@ describe('TimerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
 
+    const session: Session = {
+      sessionId: "0",
+      taskIds: ["1","2"],
+      startTime: "123456",
+      endTime: "123488"
+    }
     service.delay(10);
     service.readDataStore()
     service.save()
     console.log(service.sessionList)
-
+    service.saveSessionData(session)
     });
   });
+});
